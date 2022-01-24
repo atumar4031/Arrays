5b24b7f67ec616e5435d27fccbb040265e8fbc84
Almustapha <almusataphatukur111@gmail.com>
2022-01-24 10:20:43 +0100

    Adding two dimentional array feature

--- src/org/arrays/Main.java
+++ src/org/arrays/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        String numbers = one_d_arrays(5);
-        System.out.println(numbers);
-
+//        String numbers = one_d_arrays(5);
+//        System.out.println(numbers);
+        two_d_arrays();
 
     }
     public static String one_d_arrays(int size){
@@ -22,5 +22,18 @@ public class Main {
         output += "]";
 
         return output;
+    }
+    public static void two_d_arrays(){
+        String[][] fruits = new String[3][3];
+
+        for (int row = 0; row < fruits.length; row++){
+            System.out.print("[");
+            for (int col = 0; col < fruits[row].length ; col++){
+                fruits[row][col] = row + "";
+                System.out.print(fruits[row][col]+" ");
+            }
+        System.out.print("]");
+        System.out.println();
+        }
     }
 }
